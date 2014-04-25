daa5ea94195c5f000a826b860ae6462ee3bea41c
Michael Tracy <slightlycuban@gmail.com>
2014-04-24 23:40:47 -0400

    Start stubbing out keyboard events.

---
+++ js/keyboard.js
@@ -0,0 +1,16 @@
+LEFT = 37;
+UP = 38;
+RIGHT = 39;
+DOWN = 40;
+
+function checkKey(e) {
+  e = e || window.event;
+
+  if (e.keycode == LEFT) {
+    // rotate camera left
+  }
+
+  if (e.keycode == RIGHT) {
+    // rotate camera right
+  }
+}
